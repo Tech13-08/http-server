42f33fe9ad3ab58fb4f360083efbc19f8eba6673
Tech13-08 <falaktulsi@gmail.com>
2024-06-24 00:14:44 -0700

    echo endpoint

--- app/main.js
+++ app/main.js
@@ -22,7 +22,19 @@ const server = net.createServer((socket) => {
       switch (dataString.substring(0, pathStartIndex - 1)) {
         case "GET":
           if (pathData[0].length > 0) {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            switch (pathData[0]) {
+              case "echo":
+                let content = pathData[1];
+                socket.write(
+                  "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
+                    content.length +
+                    "\r\n\r\n" +
+                    content
+                );
+                break;
+              default:
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
           } else {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
           }
